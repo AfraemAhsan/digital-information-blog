ea7511aa0de900914ffd9db17affbf7e3b969431
afraemahsan <afraemahsan24@gmail.com>
2022-09-03 20:10:12 +0600

    blog next update commit

--- app.js
+++ app.js
@@ -44,9 +44,24 @@ const ShowBlogs = (datas) => {
       <div class="w-[244px] h-[300px]">
       <img src="${data.image_url}" class="w-full h-full object-cover">
       </div>    
-      <div class="w-[886px] h-[200px] float-right ml-[280px] mt-[-250px]">
+      <div class="w-[886px] h-[200px] float-right ml-[280px] mt-[-300px]">
       <h1 class="font-bold text-3xl">${data.title}</h1>
       <p>${data.details}</p>
+      </div>
+      <div class="flex flex-row justify-between space-around ml-[300px] mt-[-40px]">
+      <div class="flex flex-row space-x-[15px]" >
+      <img src="${data.author.img}" class="w-[40px] h-[40px] object-cover" >
+      <div>
+      <h5>${data.author.name}</h5>
+      <span>${data.author.published_date}</span>
+      </div>
+    
+      
+      <span class=" px-[100px] flex  flex-row space-y-[20px] mt-[10px]"><img src="carbon_view.png" class="cursor-pointer mr-[15px] mt-[5px] w-[30px] h-[20px]  object-cover align-center  ">${data.total_view}</span>
+      
+      </div>
+      
+      
       </div>
     </div>`;
 
