7b893c2ec3164a91c958a9769098fd6d62cd219a
afraemahsan <afraemahsan24@gmail.com>
2022-09-03 15:20:17 +0600

    blog update commit

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const loadData = () => {
       console.log(err);
     });
 };
+
 loadData();
 const displayData = (bloglists) => {
   const blogContainer = document.getElementById("blog-container");
@@ -20,7 +21,7 @@ const displayData = (bloglists) => {
     console.log(blog);
     const listContainer = document.createElement("div");
     listContainer.innerHTML = `
-     <li class="cursor-pointer list-none float-left px-[20px] py-[20px] space-between ml-[50px]">${blog.category_name}</li>
+     <li class="cursor-pointer list-none float-left px-[20px] py-[20px] space-between ml-[50px] ">${blog.category_name}</li>
      `;
 
     blogContainer.appendChild(listContainer);
@@ -39,7 +40,17 @@ const ShowBlogs = (datas) => {
   const showBlog = document.getElementById("show-blog");
   datas.forEach((data) => {
     const blogWrapper = document.createElement("div");
+    blogWrapper.innerHTML = `<div class="w-[1170px] grid grid-cols-1 mx-auto my-[30px]">
+      <div class="w-[244px] h-[300px]">
+      <img src="${data.image_url}" class="w-full h-full object-cover">
+      </div>    
+      <div class="w-[886px] h-[200px] float-right ml-[280px] mt-[-250px]">
+      <h1 class="font-bold text-3xl">${data.title}</h1>
+      <p>${data.details}</p>
+      </div>
+    </div>`;
 
+    showBlog.appendChild(blogWrapper);
     console.log(data);
   });
 };
